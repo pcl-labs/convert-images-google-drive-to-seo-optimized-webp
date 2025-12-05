e80791be774faf42da1136a384bf932d56435996
Paul Chris Luke <paulchrisluke@gmail.com>
2025-12-05 17:28:16 +0700

    Update auth implementation

--- src/workers/api/constants.py
+++ src/workers/api/constants.py
@@ -2,5 +2,5 @@
 API-level constants.
 """
 
-COOKIE_OAUTH_STATE = "oauth_state"
-COOKIE_GOOGLE_OAUTH_STATE = "google_oauth_state"
+# COOKIE_OAUTH_STATE removed - no longer used (GitHub OAuth login removed)
+COOKIE_GOOGLE_OAUTH_STATE = "google_oauth_state"  # Still used for Google integration linking
